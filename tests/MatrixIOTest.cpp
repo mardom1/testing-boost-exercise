c167eb2e361f4e24d609a75a98fe0adba8c2ab50
Marcel Marquardt <marcel.marquardt@hpe.com>
2023-02-02 16:41:40 +0100

    Run clang-format

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -10,8 +10,8 @@ BOOST_AUTO_TEST_CASE(LoadMatrix)
 {
   MatrixXd expected(3, 3);
   expected << 1, 2, -3,
-              -6, 7.5, 8,
-              0, -0, -0.12;
+      -6, 7.5, 8,
+      0, -0, -0.12;
 
   MatrixXd matrix = matrixIO::openData("testmatrix.csv", 3);
   BOOST_TEST(expected == matrix);
