6fc8b3f98bd746a9e80daa86269af2f6395244d1
Marcel Marquardt <marcel.marquardt@hpe.com>
2023-02-02 17:11:23 +0100

    Test element-wise

--- tests/MatrixIOTest.cpp
+++ tests/MatrixIOTest.cpp
@@ -14,7 +14,15 @@ BOOST_AUTO_TEST_CASE(LoadMatrix)
       0, -0, -0.12;
 
   MatrixXd matrix = matrixIO::openData("testmatrix.csv", 3);
-  BOOST_TEST(expected == matrix);
+  BOOST_TEST(expected(0, 0) == matrix(0, 0));
+  BOOST_TEST(expected(0, 1) == matrix(0, 1));
+  BOOST_TEST(expected(0, 2) == matrix(0, 2));
+  BOOST_TEST(expected(1, 0) == matrix(1, 0));
+  BOOST_TEST(expected(1, 1) == matrix(1, 1));
+  BOOST_TEST(expected(1, 2) == matrix(1, 2));
+  BOOST_TEST(expected(2, 0) == matrix(2, 0));
+  BOOST_TEST(expected(2, 1) == matrix(2, 1));
+  BOOST_TEST(expected(2, 2) == matrix(2, 2));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
